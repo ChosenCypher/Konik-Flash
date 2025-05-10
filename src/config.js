991582f4fbcca9bee141f51724c73097e3f50bcf
ChosenCypher <hugs@duck.com>
2025-05-10 16:26:52 +0300

    agnos 11.13

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
   manifests: {
-    release: 'https://raw.githubusercontent.com/hoofpilot/hoofpilot/refs/heads/hoofpilot-release3/system/hardware/tici/agnos.json',
-    master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
+    release: 'https://r2.openpilot.my.id/agnosupdate/11.13/agnos.json',
   },
   loader: {
     url: 'https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin',
